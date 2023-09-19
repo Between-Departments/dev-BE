699ef2fadbca51a3955a0bfbbc460324ff0b8224
Jeong min <tjrwjdals7@naver.com>
2023-09-19 23:54:48 +0900

    init: spring boot 프로젝트 설정

---
+++ src/test/java/com/gwakkili/devbe/DevBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwakkili.devbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
