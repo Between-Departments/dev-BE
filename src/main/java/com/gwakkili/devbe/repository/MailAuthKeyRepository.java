9dc2ed0d534fb25cdd5aaa2bc5e35e64534917c9
Jeong min <tjrwjdals7@naver.com>
2023-09-25 18:30:29 +0900

    Feature: MailAuthKeyRepository 추가

---
+++ src/main/java/com/gwakkili/devbe/repository/MailAuthKeyRepository.java
@@ -0,0 +1,7 @@
+package com.gwakkili.devbe.repository;
+
+import com.gwakkili.devbe.entity.MailAuthKey;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MailAuthKeyRepository extends CrudRepository<MailAuthKey, String> {
+}
