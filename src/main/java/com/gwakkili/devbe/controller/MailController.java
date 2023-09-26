847f06cfa18eac4228badff357cd8d9d0462ee23
Jeong min <tjrwjdals7@naver.com>
2023-09-26 18:25:38 +0900

    Feature: MailController 추가

---
+++ src/main/java/com/gwakkili/devbe/controller/MailController.java
@@ -0,0 +1,28 @@
+package com.gwakkili.devbe.controller;
+
+import com.gwakkili.devbe.service.MailService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/members/mail")
+public class MailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/send")
+    public void send(String mail) throws MessagingException, UnsupportedEncodingException {
+        mailService.send(mail);
+    }
+
+    @GetMapping("/confirm")
+    public boolean confirmAuthKey(String mail, String authKey){
+        return mailService.checkAuthKey(mail, authKey);
+    }
+}
