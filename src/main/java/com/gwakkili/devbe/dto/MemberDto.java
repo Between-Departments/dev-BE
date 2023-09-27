d959345b5e762e9887e05bbddf07e98917e37639
Jeong min <tjrwjdals7@naver.com>
2023-09-27 22:58:26 +0900

    Feature: 유효성 검사 어노테이션 추가

--- src/main/java/com/gwakkili/devbe/dto/MemberDto.java
+++ src/main/java/com/gwakkili/devbe/dto/MemberDto.java
@@ -9,13 +9,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
-@PasswordConfirm(fieldName1 = "password", fieldName2 = "passwordConfirm")
 public class MemberDto {
 
     @Data
     @AllArgsConstructor
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @Builder
+    @PasswordConfirm(fieldName1 = "password", fieldName2 = "passwordConfirm")
     public static class Save{
 
         @Email
@@ -24,11 +24,13 @@ public class MemberDto {
         private String mail;
 
         @NotBlank
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$",
+                 message = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String password;
 
         @NotBlank
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$",
+                message = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String passwordConfirm;
 
         @NotBlank
