e91b83b588cec2e6f6d8e660d915bd27f66cc2a8
Jeong min <tjrwjdals7@naver.com>
2023-09-28 22:12:31 +0900

    Refactor: 클래스 구조 변경

--- src/test/java/com/gwakkili/devbe/DevBeApplicationTests.java
+++ src/test/java/com/gwakkili/devbe/DevBeApplicationTests.java
@@ -1,14 +1,28 @@
 package com.gwakkili.devbe;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gwakkili.devbe.config.DummyDataProvider;
+import com.gwakkili.devbe.config.S3MockConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-class DevBeApplicationTests {
+@AutoConfigureMockMvc
+@Transactional
+@Import({S3MockConfig.class, DummyDataProvider.class})
+@ActiveProfiles("test")
+public class DevBeApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    protected MockMvc mockMvc;
 
-    }
+    @Autowired
+    protected ObjectMapper objectMapper;
+
 
 }
