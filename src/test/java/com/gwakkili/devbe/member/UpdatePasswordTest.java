f793a64f4148c59ad8593278dea0a619d7992de4
Jeong min <tjrwjdals7@naver.com>
2023-09-30 16:39:18 +0900

    Test: DisplayName 추가

--- src/test/java/com/gwakkili/devbe/member/UpdatePasswordTest.java
+++ src/test/java/com/gwakkili/devbe/member/UpdatePasswordTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DisplayName("비밀번호 변경 테스트")
 public class UpdatePasswordTest extends DevBeApplicationTests {
 
     @Test
