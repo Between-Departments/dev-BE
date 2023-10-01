0c704fb58d5edc91faf21a3dbc33afae05c68778
Jeong min <tjrwjdals7@naver.com>
2023-10-01 18:23:24 +0900

    Feature: 키워드 검색 추가

--- src/main/java/com/gwakkili/devbe/member/repository/MemberRepository.java
+++ src/main/java/com/gwakkili/devbe/member/repository/MemberRepository.java
@@ -1,10 +1,15 @@
 package com.gwakkili.devbe.member.repository;
 
 import com.gwakkili.devbe.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     Optional<Member> findByMail(String mail);
+    Slice<Member> findAllByMailContaining(String mail, Pageable pageable);
 }
