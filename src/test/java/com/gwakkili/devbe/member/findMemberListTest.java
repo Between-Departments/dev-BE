50dbf7bd8f1c6e33ef5c1f4c63793eb98169a4c8
Jeong min <tjrwjdals7@naver.com>
2023-10-01 20:28:42 +0900

    Test: 권한이 없는 사용자 테스트 케이스 추가

--- src/test/java/com/gwakkili/devbe/member/findMemberListTest.java
+++ src/test/java/com/gwakkili/devbe/member/findMemberListTest.java
@@ -1,7 +1,6 @@
 package com.gwakkili.devbe.member;
 
 import com.gwakkili.devbe.DevBeApplicationTests;
-import com.gwakkili.devbe.dto.SliceRequestDto;
 import com.gwakkili.devbe.util.WithMockMember;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("유저 목록조회 테스트")
 public class findMemberListTest extends DevBeApplicationTests {
 
-    String url = "/members";
+    String url = "/api/members";
 
     @Test
     @DisplayName("유저 목록조회 성공")
@@ -35,7 +34,7 @@ public class findMemberListTest extends DevBeApplicationTests {
     @WithMockMember(mail = "test@test1.ac.kr", password = "a12341234!", roles = "ROLE_MANAGER")
     public void findMemberListByKeywordTest() throws Exception{
 
-        MultiValueMap<String, String> params =new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("page", "1");
         params.add("size", "20");
         params.add("keyword", "test1.ac.kr");
@@ -43,6 +42,17 @@ public class findMemberListTest extends DevBeApplicationTests {
         //when,then
         mockMvc.perform(get(url).params(params))
                 .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("유저 목록조회 실패: 권한이 없는 사용자")
+    @WithMockMember(mail = "test@test1.ac.kr", password = "a12341234!", roles = "ROLE_USER")
+    public void findMemberListByForbidden() throws Exception {
+
+        //when,then
+        mockMvc.perform(get(url))
+                .andExpect(status().isForbidden())
                 .andDo(print());
     }
 
