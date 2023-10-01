8e447ffaaec671848ab9a9b7f73b685fb318d835
PYOSESEUNG <97ss93sh@gmail.com>
2023-10-01 14:02:10 +0900

    Fix: MailConfig PropertySource 경로 수정 + Dockerfile 수정

--- src/main/java/com/gwakkili/devbe/DevBeApplication.java
+++ src/main/java/com/gwakkili/devbe/DevBeApplication.java
@@ -3,13 +3,22 @@ package com.gwakkili.devbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RestController
 public class DevBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevBeApplication.class, args);
+    }
+
+
+    @GetMapping
+    public String test(){
+        return "Test Success!";
     }
 
 }
