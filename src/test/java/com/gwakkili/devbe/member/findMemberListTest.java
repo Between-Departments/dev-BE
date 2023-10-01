46b348fdf6ed2e0bb3c7bd2c9db2ede7fa094151
Jeong min <tjrwjdals7@naver.com>
2023-10-01 18:24:34 +0900

    Test: 회원목록 조회기능 테스트 추가

---
+++ src/test/java/com/gwakkili/devbe/member/findMemberListTest.java
@@ -0,0 +1,45 @@
+package com.gwakkili.devbe.member;
+
+import com.gwakkili.devbe.DevBeApplicationTests;
+import com.gwakkili.devbe.dto.SliceRequestDto;
+import com.gwakkili.devbe.util.WithMockMember;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@DisplayName("유저 목록조회 테스트")
+public class findMemberListTest extends DevBeApplicationTests {
+
+    String url = "/members";
+
+    @Test
+    @DisplayName("유저 목록조회 성공")
+    @WithMockMember(mail = "test@test1.ac.kr", password = "a12341234!", roles = "ROLE_MANAGER")
+    public void findMemberListTest() throws Exception{
+
+        //when,then
+        mockMvc.perform(get(url))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("유저 목록조회 성공: 키워드 검색")
+    @WithMockMember(mail = "test@test1.ac.kr", password = "a12341234!", roles = "ROLE_MANAGER")
+    public void findMemberListByKeywordTest() throws Exception{
+
+        MultiValueMap<String, String> params =new LinkedMultiValueMap<>();
+        params.add("page", "1");
+        params.add("size", "20");
+        params.add("keyword", "test1.ac.kr");
+
+        //when,then
+        mockMvc.perform(get(url).params(params))
+                .andDo(print());
+    }
+
+}
