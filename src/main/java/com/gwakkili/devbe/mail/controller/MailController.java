79e4e88fc9caa7336f6f0439b96d5df6af796c1a
Jeong min <tjrwjdals7@naver.com>
2023-10-02 14:46:08 +0900

    Feature: 메일 유효성 검증 추가

--- src/main/java/com/gwakkili/devbe/mail/controller/MailController.java
+++ src/main/java/com/gwakkili/devbe/mail/controller/MailController.java
@@ -2,8 +2,11 @@ package com.gwakkili.devbe.mail.controller;
 
 import com.gwakkili.devbe.mail.service.MailService;
 import jakarta.mail.MessagingException;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
@@ -11,18 +14,19 @@ import java.io.UnsupportedEncodingException;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/members/mail")
+@RequestMapping("/api/members/mail")
+@Validated
 public class MailController {
 
     private final MailService mailService;
 
     @PostMapping("/send")
-    public void send(String mail) throws MessagingException, UnsupportedEncodingException {
+    public void send(@Email @RequestParam(value = "mail") String mail) throws MessagingException, UnsupportedEncodingException {
         mailService.send(mail);
     }
 
     @GetMapping("/confirm")
-    public boolean confirmAuthKey(String mail, String authKey){
+    public boolean confirmAuthKey(@RequestParam @Email String mail, String authKey) {
         return mailService.checkAuthKey(mail, authKey);
     }
 }
