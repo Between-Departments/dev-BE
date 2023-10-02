0f839eae57939f31b7acfc9a77677379bf1b8b79
Jeong min <tjrwjdals7@naver.com>
2023-10-02 14:45:43 +0900

    Feature: 메일 전송실패 코드와 예외 처리 추가

--- src/main/java/com/gwakkili/devbe/exception/ControllerExceptionHandler.java
+++ src/main/java/com/gwakkili/devbe/exception/ControllerExceptionHandler.java
@@ -4,9 +4,12 @@ import com.gwakkili.devbe.exception.dto.ExceptionDto;
 import com.gwakkili.devbe.exception.customExcption.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Set;
 
 @Slf4j
 @ControllerAdvice
@@ -22,10 +25,20 @@ public class ControllerExceptionHandler {
 
     //바인딩 에러시 발생
     @ExceptionHandler
-    public ResponseEntity<ExceptionDto> bindExceptionHandler(BindException exception){
+    public ResponseEntity<ExceptionDto> bindExceptionHandler(BindException exception) {
         log.error(exception.toString());
         ExceptionCode code = ExceptionCode.INVALID_INPUT_VALUE;
         ExceptionDto exceptionDto = new ExceptionDto(code, exception.getBindingResult());
         return ResponseEntity.status(code.getHttpStatus()).body(exceptionDto);
     }
+
+    //메일 전송 실패
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDto> mailExceptionHandler(MailException exception) {
+        log.error(exception.toString());
+        ExceptionCode code = ExceptionCode.FAIL_SEND_MAIL;
+        ExceptionDto exceptionDto = new ExceptionDto(ExceptionCode.FAIL_SEND_MAIL);
+        return ResponseEntity.status(code.getHttpStatus()).body(exceptionDto);
+    }
+
 }
