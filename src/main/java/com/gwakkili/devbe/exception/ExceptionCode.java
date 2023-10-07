be6014efef747ceb3e30e3ba9b567bb50c255912
Jeong min <tjrwjdals7@naver.com>
2023-10-07 22:35:42 +0900

    Feature: NOT_FOUND_REPLY 추가

--- src/main/java/com/gwakkili/devbe/exception/ExceptionCode.java
+++ src/main/java/com/gwakkili/devbe/exception/ExceptionCode.java
@@ -38,7 +38,9 @@ public enum ExceptionCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "USER-001", "인증되지 않은 사용자입니다."),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "USER-003", "접근이 거부되었습니다."),
 
-    NOT_FOUND_POST(HttpStatus.NOT_FOUND,"POST-001" ,"게시물을 찾을 수 없습니다." );
+    NOT_FOUND_POST(HttpStatus.NOT_FOUND, "POST-001", "게시물을 찾을 수 없습니다."),
+
+    NOT_FOUND_REPLY(HttpStatus.NOT_FOUND, "REPLY-001", "댓글을 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
