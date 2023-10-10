0e8795fe6a3d30c4e054ab3dd618e6509484be74
Jeong min <tjrwjdals7@naver.com>
2023-10-10 16:22:29 +0900

    Style: 클래스 이름 변경

--- src/test/java/com/gwakkili/devbe/image/ImageTests.java
+++ src/test/java/com/gwakkili/devbe/image/ImageTests.java
@@ -23,8 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-public class ImageControllerTest  extends DevBeApplicationTests {
-
+public class ImageTests extends DevBeApplicationTests {
 
 
     private static final String BUCKET_NAME = "test-bucket";
