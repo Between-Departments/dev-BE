f89ef8100491562c957f5f34451095121794a07b
jeong min <121369135+jeongmin0709@users.noreply.github.com>
2023-10-26 22:31:52 +0900

    Fix: getter가 없어서 josonString으로 변환되지 않던 문제

--- src/main/java/com/gwakkili/devbe/dto/ListResponseDto.java
+++ src/main/java/com/gwakkili/devbe/dto/ListResponseDto.java
@@ -2,11 +2,18 @@ package com.gwakkili.devbe.dto;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@Data
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema
 public class ListResponseDto<DTO, EN> {
 
     @Schema(description = "데이터 리스트")
