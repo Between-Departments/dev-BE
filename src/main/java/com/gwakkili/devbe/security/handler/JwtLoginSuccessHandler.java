83b969d815be6f54dc7482fa34ff523ff5f3b16f
PYOSESEUNG <97ss93sh@gmail.com>
2023-10-29 23:02:14 +0900

    Fix : 쿠키 반환 도메인 수정

--- src/main/java/com/gwakkili/devbe/security/handler/JwtLoginSuccessHandler.java
+++ src/main/java/com/gwakkili/devbe/security/handler/JwtLoginSuccessHandler.java
@@ -28,6 +28,7 @@ public class JwtLoginSuccessHandler implements AuthenticationSuccessHandler {
         String refreshToken = jwtService.generateRefreshToken(memberDetails);
 
         ResponseCookie responseCookie = ResponseCookie.from("RefreshToken", refreshToken)
+                .domain("https://dev-fe-between.vercel.app/")
                 .path("/api/refresh")
                 .maxAge((int) jwtService.getRefreshTokenExpireTime() / 1000)
                 .httpOnly(true)
