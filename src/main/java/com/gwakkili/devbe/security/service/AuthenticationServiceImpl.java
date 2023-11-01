f70e3c1e3658df82b43024bf9b8274228d033cab
jeong min <121369135+jeongmin0709@users.noreply.github.com>
2023-11-01 19:21:29 +0900

    Refactor: 인증 예외처리 간소화

--- src/main/java/com/gwakkili/devbe/security/service/AuthenticationServiceImpl.java
+++ src/main/java/com/gwakkili/devbe/security/service/AuthenticationServiceImpl.java
@@ -1,11 +1,9 @@
 package com.gwakkili.devbe.security.service;
 
-import com.gwakkili.devbe.exception.ExceptionCode;
-import com.gwakkili.devbe.exception.customExcption.JwtException;
 import com.gwakkili.devbe.security.dto.JwtTokenDto;
 import com.gwakkili.devbe.security.dto.LoginDto;
-import com.gwakkili.devbe.security.dto.MemberDetails;
-import com.gwakkili.devbe.security.entity.RefreshToken;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,46 +21,33 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public JwtTokenDto login(LoginDto loginDto) throws AuthenticationException {
+
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginDto.getMail(), loginDto.getPassword());
-        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
-        MemberDetails memberDetails = (MemberDetails) authenticate.getPrincipal();
-        return jwtService.generateToken(memberDetails);
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        JwtTokenDto jwtTokenDto = jwtService.generateToken(authentication);
+        jwtService.saveRefreshToken(jwtTokenDto.getRefreshToken(), authentication);
+        return jwtTokenDto;
     }
 
     @Override
-    public void Logout(MemberDetails memberDetails) {
-        jwtService.deleteRefreshToken(memberDetails);
+    public void Logout(long memberId) {
+        jwtService.deleteRefreshToken(memberId);
     }
 
     @Override
-    public JwtTokenDto refresh(JwtTokenDto jwtTokenDto) {
+    public JwtTokenDto refresh(JwtTokenDto jwtTokenDto) throws JwtException {
 
-        String accessToken = jwtService.resolveAccessToken(jwtTokenDto.getAccessToken());
-        String refreshToken = jwtTokenDto.getRefreshToken();
-
-        String accessTokenValidate = jwtService.validateToken(accessToken);
-        String refreshTokenValidate = jwtService.validateToken(refreshToken);
-
-        if (accessTokenValidate.equals("NOT_FOUND") || refreshTokenValidate.equals("NOT_FOUND")) {
-            throw new JwtException(ExceptionCode.NOT_FOUND_TOKEN);
-        } else if (accessTokenValidate.equals("INVALID") || refreshTokenValidate.equals("INVALID")) {
-            throw new JwtException(ExceptionCode.INVALID_TOKEN);
-        } else if (refreshTokenValidate.equals("EXPIRE")) {
-            throw new JwtException(ExceptionCode.EXPIRED_TOKEN);
+        try {
+            jwtService.getAuthenticationByAccessToken(jwtTokenDto.getAccessToken());
+        } catch (ExpiredJwtException ignored) {
         }
-        String mail = jwtService.getClaims(refreshToken).get("mail").toString();
 
-        RefreshToken redisRefreshToken = jwtService.getRefreshToken(mail);
-        if (redisRefreshToken == null || !redisRefreshToken.getToken().equals(refreshToken))
-            throw new JwtException(ExceptionCode.INVALID_TOKEN);
+        Authentication authentication = jwtService.getAuthenticationByRefreshToken(jwtTokenDto.getRefreshToken());
+        JwtTokenDto newJwtTokenDto = jwtService.generateToken(authentication);
+        jwtService.saveRefreshToken(newJwtTokenDto.getRefreshToken(), authentication);
+        return newJwtTokenDto;
 
-        MemberDetails memberDetails = MemberDetails.builder()
-                .mail(redisRefreshToken.getMail())
-                .roles(redisRefreshToken.getRoles())
-                .build();
-
-        return jwtService.generateToken(memberDetails);
     }
-
 }
