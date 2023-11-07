72e468349ae128ef0add1f6c01d6c70b97cba11f
jeong min <121369135+jeongmin0709@users.noreply.github.com>
2023-11-07 21:17:47 +0900

    Feature: 기본생성자 추가

--- src/main/java/com/gwakkili/devbe/mail/dto/MailSendDto.java
+++ src/main/java/com/gwakkili/devbe/mail/dto/MailSendDto.java
@@ -2,14 +2,17 @@ package com.gwakkili.devbe.mail.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MailSendDto {
 
     @NotBlank
     @Email
-    String mail;
+    private String mail;
 }
