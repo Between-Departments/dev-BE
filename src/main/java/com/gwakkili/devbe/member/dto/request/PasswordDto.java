d2126a4182c9c171fc3a1b5d4cfb87052c0c0264
jeong min <121369135+jeongmin0709@users.noreply.github.com>
2023-11-08 23:33:55 +0900

    Fix: 기본생성자 추가

--- src/main/java/com/gwakkili/devbe/member/dto/request/PasswordDto.java
+++ src/main/java/com/gwakkili/devbe/member/dto/request/PasswordDto.java
@@ -3,11 +3,14 @@ package com.gwakkili.devbe.member.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PasswordDto {
 
     @NotBlank
