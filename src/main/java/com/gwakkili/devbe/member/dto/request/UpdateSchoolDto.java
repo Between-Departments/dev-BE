30cc4026421d42f8d4560205651472eb22c4a5f1
jeong min <121369135+jeongmin0709@users.noreply.github.com>
2023-11-13 00:15:56 +0900

    Feature: mail NotBlank 유효성 검사 추가

--- src/main/java/com/gwakkili/devbe/member/dto/request/UpdateSchoolDto.java
+++ src/main/java/com/gwakkili/devbe/member/dto/request/UpdateSchoolDto.java
@@ -7,6 +7,7 @@ import com.gwakkili.devbe.validation.annotation.MailNotAuth;
 import com.gwakkili.devbe.validation.annotation.School;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -17,13 +18,15 @@ public class UpdateSchoolDto {
     @JsonIgnore
     long memberId;
 
-    @MailDuplicate
-    @MailNotAuth
     @Email
+    @NotBlank
+    @MailNotAuth
+    @MailDuplicate
     @Schema(description = "새로운 메일", example = "test1@sun.ac.kr")
     private String newMail;
 
     @School
+    @NotBlank
     @Schema(description = "학교", example = "연세대학교")
     private String school;
 
